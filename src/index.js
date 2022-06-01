6394fd8d25b418617373b493797efb778addf925
James Glendinning <james@glenavontechnology.co.uk>
2022-06-01 22:36:15 +0100

    initial config

--- src/index.js
+++ src/index.js
@@ -4,9 +4,25 @@ let table;
 //https://datatables.net/examples/index
 
 window.loadTable = (json) => {
-  const columns = [];
-  const data = [];
+  const columns = [
+    {title: "First Name", data: "firstName"},
+    {title: "Last Name", data: "lastName"},
+    {title: "Position", data: "position", searchable: false},
+  ];
+  const data = [
+    {firstName: "James", lastName: "Glendinning", position: "Big Chief", id: "DSHFLJGLDKFHG"},
+    {firstName: "George", lastName: "Glendinning", position: "Big Dog"},
+    {firstName: "Kat", lastName: "Glendinning", position: "Top Lady"}
+];
+
   table = $("#dtable").DataTable({
+  //  paging: false,
+  //  ordering: false,
+    searching: true,
+    dom: 'B<"clear">lfrtip',
+    buttons: true,
+
+
     columns: columns,
     data: data,
   });
